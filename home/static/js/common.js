1f61d55502818413e114580c7b4abcdc4c7d1a94
MyAngeler <1543712692@qq.com>
2018-06-22 19:06:11 +0800

    产品详情页

---
+++ home/static/js/common.js
@@ -0,0 +1,18 @@
+/*
+* 打开弹窗
+* @param id 弹窗名字
+* @author hy 2018-06-22
+* */
+function openPopWin (id , _area){
+    int_layer_index=layer.open({
+        type: 2,//frame层
+        shadeClose: true,
+        title: '',
+        shade: 0.4,
+        area: _area,
+        anim: 5,
+        resize: false,
+        scrollbar: false,
+        content: ['pop/' + id + '.html' , 'no'],
+    });
+}
